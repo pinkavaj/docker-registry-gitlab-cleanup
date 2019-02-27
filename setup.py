1e6e3fa35cef4733edcf13125f3f65ce86e3d3a6
Jiri Pinkava <jiri.pinkava@rossum.ai>
2019-02-27 02:17:09 +0100

    cleanup using Gitlab registry API

--- setup.py
+++ setup.py
@@ -2,23 +2,22 @@ from setuptools import setup, find_packages
 from rgc.version import __version__
 
 setup(
-  name             = 'rgc',
-  version          = __version__,
-  description      = 'Cleanup old tags from docker-registry provided with GitLab',
-  url              = 'https://github.com/mvisonneau/docker-registry-gitlab-cleanup',
-  author           = 'Maxime VISONNEAU',
-  author_email     = 'maxime.visonneau@gmail.com',
-  license          = 'AGPL-3.0',
-  packages         = find_packages( exclude=[ 'docs', 'tests*' ] ),
-  install_requires = [
-    'www_authenticate',
-    'requests',
-    'python-gitlab',
-    'termcolor'
-  ],
-  entry_points     = {
-    'console_scripts': [
-      'rgc=rgc.cli:main'
+    name='rgc',
+    version=__version__,
+    description='Cleanup old tags from docker-registry provided with GitLab',
+    url='https://github.com/mvisonneau/docker-registry-gitlab-cleanup',
+    author='Maxime VISONNEAU, Jiří Pinkava',
+    author_email='maxime.visonneau@gmail.com, j-pi@seznam.cz',
+    license='AGPL-3.0',
+    packages=find_packages(exclude=['docs', 'tests*']),
+    install_requires=[
+        'requests',
+        'python-gitlab>=1.8.0',
+        'termcolor'
     ],
-  }
+    entry_points={
+        'console_scripts': [
+            'rgc=rgc.cli:main'
+        ],
+    }
 )
